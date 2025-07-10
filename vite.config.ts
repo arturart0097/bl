6e7e511137fea27003a66f3d12383258e76ea203
Sabetskyiii <youmaharadchu@gmail.com>
2025-07-10 14:38:04 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths"
+import { fileURLToPath } from "url";
 
-// https://vite.dev/config/
+// Емуляція __dirname для ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   resolve: {
